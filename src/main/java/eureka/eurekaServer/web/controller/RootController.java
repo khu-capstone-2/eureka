c961254fdb1d826250192e026a56dcfbef5203ec
CYY1007 <younguk86@gmail.com>
2023-12-08 21:00:18 +0900

    :tada: Set up

---
+++ src/main/java/eureka/eurekaServer/web/controller/RootController.java
@@ -0,0 +1,13 @@
+package eureka.eurekaServer.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/health")
+    public String health(){
+        return "I'm healthy!";
+    }
+}
